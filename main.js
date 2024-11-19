1187f198947ea84547125927be1fb3cc7246c2f8
Ahmed77ehab <ahmedehab30400@gmail.com>
2024-11-20 00:21:08 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// var productContainer = [];
+
+// function addQuote() {
+//   var quote = {
+//     text1: `"You miss 100% of the shots you don't take."`,
+//     text2: `"The best revenge is massive success."`,
+//     text3: `"It's not what happens to you, but how you react to it that matters."`,
+//     text4: `"Resentment is like drinking poison and waiting for your enemies to die."`,
+//   };
+
+//   productContainer.unshift(JSON.stringify(quote));
+//   document.getElementById('demo').innerHTML = productContainer;
+// }
+
+// container = [
+//   (quote = {
+//     text1: `"You miss 100% of the shots you don't take."`,
+//     text2: `"The best revenge is massive success."`,
+//     text3: `"It's not what happens to you, but how you react to it that matters."`,
+//     text4: `"Resentment is like drinking poison and waiting for your enemies to die."`,
+//   }),
+// ];
+
+// function addQuote() {
+//   container.unshift(JSON.stringify(quote))
+//   document.getElementById('demo').innerHTML = container;
+// }
